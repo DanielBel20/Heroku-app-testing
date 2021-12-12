4f4f4d4284077862fa88310d6792faa4861bebe0
DanielBel20 <91428069+DanielBel20@users.noreply.github.com>
2021-12-12 20:35:12 +0200

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -46,19 +46,3 @@ class LoginPage:
 
     def isLoginPageMessageDisplayed(self):
         return self.browser.find_element(*self.LOGIN_PAGE_MESSAGE).is_displayed()
-
-
-    # def clickAddButton(self):
-    #     self.browser.find_element(*self.ADD_ELEMENT_BUTTON).click()
-    # def clickDeleteButton(self):
-    #     self.browser.find_element(*self.DELETE_BUTTON).click()
-    #
-    # # ce incepe cu get este ok sa returneze un text
-    # def getTitlePage(self):
-    #     return self.browser.find_element(*self.TITLE).text
-    # # cand vrem sa verificam ceva cu true sau false incepem cu is
-    # def isDeleteButtonDisplayed(self):
-    #     return self.browser.find_element(*self.DELETE_BUTTON).is_displayed()
-    # #daca e get returneaza/ returneaza numarul de butoane delete care sunt pe pagina
-    # def getNumberOfDeleteButton(self):
-    #     return len(self.browser.find_elements(*self.DELETE_BUTTON))
